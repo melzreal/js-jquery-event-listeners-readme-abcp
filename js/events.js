53e63f99059c0316978bb513205512c493278f0d
Melissa Real <melissa.real@hotmail.com>
2018-03-24 00:37:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function() {
+    alert("Hey!");
+    return;
+  });
+}
+
+function frameIt(){
+ $('img').on('load', function(){
+    $('img').append('.tasty');
+
+});
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G is down');
+  }
+});
+}
+
+function submitIt(){}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
 
+  
 });
